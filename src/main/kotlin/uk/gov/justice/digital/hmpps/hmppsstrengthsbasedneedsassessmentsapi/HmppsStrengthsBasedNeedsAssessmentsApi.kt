939ce54ebf94b6d7499b117eb46a10d12e9d77c9
Yasmin-jones <Yasmin-jones@users.noreply.github.com>
2023-04-04 09:51:55 +0000

    updating project name after deployment from template repository

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/HmppsStrengthsBasedNeedsAssessmentsApi.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication()
+class HmppsStrengthsBasedNeedsAssessmentsApi
+
+fun main(args: Array<String>) {
+  runApplication<HmppsStrengthsBasedNeedsAssessmentsApi>(*args)
+}

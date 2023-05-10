8d1622d0f20858fbc09a739d2c8693c865e95265
Lucas Cairns <lucas.cairns@gmail.com>
2023-05-10 15:31:03 +0100

    Create JPA entities and repositories

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/persistence/entity/LinkStatus.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.entity
+
+enum class LinkStatus {
+  USED,
+  UNUSED,
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/persistence/entity/Session.kt
@@ -0,0 +1,47 @@
+package uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.entity
+
+import java.io.Serializable
+import java.time.LocalDateTime
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "oasys_sessions")
+class Session(
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long? = null,
+
+  @Column(name = "uuid")
+  val uuid: UUID = UUID.randomUUID(),
+
+  @Column(name = "created_at")
+  val createdAt: LocalDateTime = LocalDateTime.now(),
+
+  @Column(name = "user_id")
+  val userSessionId: String = "",
+
+  @Column(name = "user_access")
+  val userAccess: String = "",
+
+  @Column(name = "link_status")
+  @Enumerated(EnumType.STRING)
+  var linkStatus: LinkStatus = LinkStatus.UNUSED,
+
+  @Column(name = "link_uuid")
+  val linkUuid: UUID = UUID.randomUUID(),
+
+  @ManyToOne
+  @JoinColumn(name = "assessment_uuid", referencedColumnName = "uuid")
+  val assessment: Assessment = Assessment(),
+) : Serializable

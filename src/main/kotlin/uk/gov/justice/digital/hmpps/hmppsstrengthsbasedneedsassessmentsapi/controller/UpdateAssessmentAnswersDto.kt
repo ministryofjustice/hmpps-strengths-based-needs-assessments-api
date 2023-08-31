cff2f692f94d3235c978e2e75a031d095ab1336a
Lucas Cairns <lucas.cairns@gmail.com>
2023-08-31 16:06:13 +0100

    Add collection functionality

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/controller/UpdateAssessmentAnswersDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/controller/UpdateAssessmentAnswersDto.kt
@@ -1,11 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.controller
 
-import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.entity.Answers
 
-@RestController
-@Tag(name = "Assessment Controller")
 class UpdateAssessmentAnswersDto(
   val answersToAdd: Answers = emptyMap(),
   val answersToRemove: List<String> = emptyList(),

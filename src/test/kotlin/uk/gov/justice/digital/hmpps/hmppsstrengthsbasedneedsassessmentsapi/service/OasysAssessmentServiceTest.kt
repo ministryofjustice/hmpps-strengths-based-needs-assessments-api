d74a8d4125cdb9d8b1976ef4b780b348cacd89a5
Lucas Cairns <lucas.cairns@gmail.com>
2024-02-06 15:15:16 +0000

    Add test coverage for assessment and assessment version services
    - add mockk to test dependencies

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/service/OasysAssessmentServiceTest.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.service
+
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+@DisplayName("OASys Assessment Service")
+class OasysAssessmentServiceTest {
+
+  @Test
+  fun createAssessmentWithOasysId() {
+  }
+
+  @Test
+  fun findOrCreateAssessment() {
+  }
+
+  @Test
+  fun find() {
+  }
+
+  @Test
+  fun associate() {
+  }
+}

4c796ffcda351c16130b5940fbe4c0544eea4e4c
Lucas Cairns <lucas.cairns@gmail.com>
2024-07-16 16:10:10 +0100

    Add version number to get assessment endpoint
    - Remove deprecated query params

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/persistence/criteria/AssessmentVersionCriteria.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/persistence/criteria/AssessmentVersionCriteria.kt
@@ -5,23 +5,16 @@ import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persi
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.entity.AssessmentVersion_
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.entity.Assessment_
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.entity.Tag
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
 import java.util.UUID
 
 data class AssessmentVersionCriteria(
   val assessmentUuid: UUID,
   val tags: Set<Tag>? = null,
-  val after: Long? = null,
-  val until: Long? = null,
   val versionNumber: Long? = null,
 ) {
   fun getSpecification(): Specification<AssessmentVersion> {
     return belongsToAssessment()
       .and(hasTag())
-      .and(isAfter())
-      .and(isBefore())
       .and(isVersionNumber())
   }
 
@@ -34,24 +27,6 @@ data class AssessmentVersionCriteria(
   private fun hasTag(): Specification<AssessmentVersion> {
     return Specification<AssessmentVersion> { root, _, builder ->
       tags?.takeIf { it.isNotEmpty() }?.let { builder.and(root.get(AssessmentVersion_.tag).`in`(tags)) }
-    }
-  }
-
-  private fun isAfter(): Specification<AssessmentVersion> {
-    return Specification<AssessmentVersion> { root, _, builder ->
-      after?.let {
-        val date = LocalDateTime.ofInstant(Instant.ofEpochSecond(after), ZoneId.systemDefault())
-        builder.greaterThan(root.get(AssessmentVersion_.updatedAt), date)
-      }
-    }
-  }
-
-  private fun isBefore(): Specification<AssessmentVersion> {
-    return Specification<AssessmentVersion> { root, _, builder ->
-      until?.let {
-        val date = LocalDateTime.ofInstant(Instant.ofEpochSecond(until), ZoneId.systemDefault())
-        builder.lessThan(root.get(AssessmentVersion_.updatedAt), date)
-      }
     }
   }
 

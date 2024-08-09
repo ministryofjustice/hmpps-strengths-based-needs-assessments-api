91ddd4ef7da8bb1227a78ebf3987e514515d888c
devkokov <dm.kokov@gmail.com>
2024-08-09 11:28:20 +0100

    ARN-2295 Delete unused code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/persistence/repository/AssessmentFormInfoRepository.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.entity.AssessmentFormInfo
-
-@Repository
-interface AssessmentFormInfoRepository : JpaRepository<AssessmentFormInfo, Long>

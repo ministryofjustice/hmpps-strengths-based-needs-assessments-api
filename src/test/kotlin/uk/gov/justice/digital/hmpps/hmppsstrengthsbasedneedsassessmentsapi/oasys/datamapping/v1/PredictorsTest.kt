59cf9408deb693a5af0dc18e18b6729a1b165b58
devkokov <dm.kokov@gmail.com>
2024-08-22 13:35:06 +0100

    Remove old OA data mapping prior to re-building the OA section

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/oasys/datamapping/v1/PredictorsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/oasys/datamapping/v1/PredictorsTest.kt
@@ -1,30 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.datamapping.v1
 
-import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.datamapping.Field
-import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.datamapping.Given
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.datamapping.SectionMappingTest
-import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.datamapping.Value
-import kotlin.test.Test
 
-class PredictorsTest : SectionMappingTest(Predictors(), "1.0") {
-  @Test
-  fun q30() {
-    test(
-      "o1-30",
-      Given().expect(null),
-      Given(Field.OFFENCE_ANALYSIS_ELEMENTS, emptyList()).expect(null),
-      Given(Field.OFFENCE_ANALYSIS_GAIN, emptyList()).expect(null),
-      Given(Field.OFFENCE_ANALYSIS_ELEMENTS, emptyList())
-        .and(Field.OFFENCE_ANALYSIS_GAIN, emptyList())
-        .expect(null),
-      Given(Field.OFFENCE_ANALYSIS_ELEMENTS, listOf(Value.SEXUAL_ELEMENT)).expect("YES"),
-      Given(Field.OFFENCE_ANALYSIS_GAIN, listOf(Value.SEXUAL_DESIRES)).expect("YES"),
-      Given(Field.OFFENCE_ANALYSIS_ELEMENTS, emptyList())
-        .and(Field.OFFENCE_ANALYSIS_GAIN, listOf(Value.SEXUAL_DESIRES))
-        .expect("YES"),
-      Given(Field.OFFENCE_ANALYSIS_ELEMENTS, listOf(Value.SEXUAL_ELEMENT))
-        .and(Field.OFFENCE_ANALYSIS_GAIN, emptyList())
-        .expect("YES"),
-    )
-  }
-}
+class PredictorsTest : SectionMappingTest(Predictors(), "1.0")

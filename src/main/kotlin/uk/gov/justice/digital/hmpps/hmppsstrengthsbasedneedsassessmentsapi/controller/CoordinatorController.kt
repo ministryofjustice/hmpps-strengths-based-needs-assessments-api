225eb6be295d4862bd548ca0cb1a43a9b9fab4a0
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-10-07 14:07:05 +0100

    SP2-791: Add coordinator lock and sign by assessment uuid (#266)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/controller/CoordinatorController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/controller/CoordinatorController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,8 +19,12 @@ import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.controller.request.CreateAssessmentRequest
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.controller.response.CreateAssessmentResponse
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.controller.response.ErrorResponse
+import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.controller.request.AuditedRequest
+import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.controller.request.SignAssessmentRequest
+import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.controller.response.OasysAssessmentResponse
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.oasys.controller.response.OasysAssessmentVersionResponse
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.criteria.AssessmentVersionCriteria
+import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.persistence.entity.UserDetails
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.service.AssessmentService
 import uk.gov.justice.digital.hmpps.hmppsstrengthsbasedneedsassessmentsapi.service.AssessmentVersionService
 import java.util.*
@@ -74,5 +79,80 @@ class CoordinatorController(
     return AssessmentVersionCriteria(assessmentUuid)
       .run(assessmentVersionService::find)
       .run(OasysAssessmentVersionResponse::from)
+  }
+
+  @RequestMapping(path = ["/{assessmentUuid}/sign"], method = [RequestMethod.POST])
+  @Operation(description = "Signs the latest version of an assessment identified by the provided Assessment UUID")
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "200", description = "Assessment version signed successfully"),
+      ApiResponse(
+        responseCode = "404",
+        description = "Assessment not found",
+        content = arrayOf(Content(schema = Schema(implementation = ErrorResponse::class))),
+      ),
+      ApiResponse(
+        responseCode = "409",
+        description = "The assessment could not be signed. See details in error message.",
+        content = arrayOf(Content(schema = Schema(implementation = ErrorResponse::class))),
+      ),
+      ApiResponse(
+        responseCode = "500",
+        description = "Unexpected error",
+        content = arrayOf(Content(schema = Schema(implementation = ErrorResponse::class))),
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('ROLE_STRENGTHS_AND_NEEDS_OASYS', 'ROLE_STRENGTHS_AND_NEEDS_WRITE')")
+  fun sign(
+    @Parameter(description = "Assessment UUID", required = true, example = "123e4567-e89b-12d3-a456-426614174000")
+    @PathVariable
+    assessmentUuid: UUID,
+    @RequestBody
+    @Valid
+    request: SignAssessmentRequest,
+  ): OasysAssessmentResponse {
+    return AssessmentVersionCriteria(assessmentUuid)
+      .run(assessmentVersionService::find)
+      .let {
+        assessmentVersionService.sign(it, UserDetails.from(request), request.signType)
+      }
+      .run(OasysAssessmentResponse::from)
+  }
+
+  @RequestMapping(path = ["/{assessmentUuid}/lock"], method = [RequestMethod.POST])
+  @Operation(description = "Locks the latest version of an assessment identified by the provided Assessment UUID")
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "200", description = "Assessment version locked successfully"),
+      ApiResponse(
+        responseCode = "404",
+        description = "Assessment not found",
+        content = arrayOf(Content(schema = Schema(implementation = ErrorResponse::class))),
+      ),
+      ApiResponse(
+        responseCode = "409",
+        description = "The latest version of the assessment has already been locked",
+        content = arrayOf(Content(schema = Schema(implementation = ErrorResponse::class))),
+      ),
+      ApiResponse(
+        responseCode = "500",
+        description = "Unexpected error",
+        content = arrayOf(Content(schema = Schema(implementation = ErrorResponse::class))),
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('ROLE_STRENGTHS_AND_NEEDS_OASYS', 'ROLE_STRENGTHS_AND_NEEDS_WRITE')")
+  fun lock(
+    @Parameter(description = "Assessment UUID", required = true, example = "123e4567-e89b-12d3-a456-426614174000")
+    @PathVariable
+    assessmentUuid: UUID,
+    @RequestBody @Valid
+    request: AuditedRequest,
+  ): OasysAssessmentResponse {
+    return AssessmentVersionCriteria(assessmentUuid)
+      .run(assessmentVersionService::find)
+      .let { assessmentVersionService.lock(it, UserDetails.from(request)) }
+      .run(OasysAssessmentResponse::from)
   }
 }

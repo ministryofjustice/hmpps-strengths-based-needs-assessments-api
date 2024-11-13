8e1efd1fe91ad60be04b97b399053f2e0c3c0f85
Dimitar Kokov <dimitar.kokov@digital.justice.gov.uk>
2024-11-13 10:54:27 +0000

    Increase max user ID length (#292)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/config/Constraints.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstrengthsbasedneedsassessmentsapi/config/Constraints.kt
@@ -5,7 +5,7 @@ class Constraints {
     const val OASYS_PK_MAX_LENGTH = 15
     const val OASYS_PK_MIN_LENGTH = 1
     const val REGION_PRISON_CODE_MAX_LENGTH = 15
-    const val OASYS_USER_ID_MAX_LENGTH = 15
+    const val OASYS_USER_ID_MAX_LENGTH = 30
     const val OASYS_USER_NAME_MAX_LENGTH = 64
   }
 }
